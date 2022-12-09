dc801d357219e0c1c944819de609ba9516cb533d
Ricardo Lima <ric.lima1@hotmail.com>
2022-12-09 01:23:39 +0000

    Sidebar
    sidebar with tabs

--- myProject/src/app/app.module.ts
+++ myProject/src/app/app.module.ts
@@ -1,34 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
-}
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot({ mode: 'ios' }),
-    AppRoutingModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: createTranslateLoader,
-        deps: [HttpClient],
-      },
-    }),
-  ],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ myProject/src/app/tabs/tabs.page.ts
@@ -0,0 +1,82 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { TabBarService } from '../tab-bar.service';
+
+const TAB_PAGES: any[] = [
+  {
+    title: 'Tab 1',
+    tab: 'tab1',
+    icon: 'search',
+    inSidemenu: true,
+    inTabBar: false,
+    showTabBar: false,
+  },
+  {
+    title: 'Tab 2',
+    tab: 'tab2',
+    icon: 'information-circle',
+    inSidemenu: true,
+    inTabBar: false,
+    showTabBar: true,
+  },
+  {
+    title: 'Tab 3',
+    tab: 'tab3',
+    icon: 'triangle',
+    inSidemenu: false,
+    inTabBar: true,
+    showTabBar: true,
+  },
+  {
+    title: 'Tab 4',
+    tab: 'tab4',
+    icon: 'square',
+    inSidemenu: false,
+    inTabBar: true,
+    showTabBar: true,
+  },
+  {
+    title: 'Tab 5',
+    tab: 'tab5',
+    icon: 'ellipse',
+    inSidemenu: false,
+    inTabBar: true,
+    showTabBar: false,
+  },
+];
+
+/*
+ * TABS_ROOT is the root path of all pages, e.g, if you set this
+ * to 'app' then all pages start with the path 'app' as in:
+ * http://localhost:8100/app/tab1.
+ */
+export const TABS_ROOT: string = 'tabs';
+export const APP_PAGES: any[] = TAB_PAGES.map((page: any) => {
+  page.url = '/' + TABS_ROOT + '/' + page.tab;
+  return page;
+});
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss'],
+})
+export class TabsPage {
+  public readonly tabBarPages: any = TAB_PAGES.filter(
+    (page: any) => page.inTabBar
+  );
+
+  @ViewChild('tabBar', { read: ElementRef, static: false })
+  private tabBarRef!: ElementRef;
+
+  constructor(private tabBarService: TabBarService) {}
+
+  public ngAfterViewInit(): void {
+    const pagesShowingTabBar: Set<string> = new Set<string>(
+      TAB_PAGES.filter((page: any) => page.showTabBar).map(
+        (page: any) => page.tab
+      )
+    );
+    this.tabBarService.init(this.tabBarRef, pagesShowingTabBar);
+  }
+}
