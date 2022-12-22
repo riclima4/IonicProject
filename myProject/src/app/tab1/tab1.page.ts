c0ec5d4bbe65d2543bf6005c6ffda71c71b86016
Ricardo Lima <ric.lima1@hotmail.com>
2022-12-22 02:16:05 +0000

    Homepage
    Adicionar secções e mexer com cores

--- myProject/src/app/tab1/tab1.page.ts
+++ myProject/src/app/tab1/tab1.page.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
+  toggleTheme(event: any) {
+    if (event.detail.checked) {
+      document.body.setAttribute('color-theme', 'dark');
+    } else {
+      document.body.setAttribute('color-theme', 'light');
+    }
+  }
 }
