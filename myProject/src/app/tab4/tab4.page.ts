2a9f46bf6b5a1432c315f8850d5ceef6edfe2570
Ricardo Lima <ric.lima1@hotmail.com>
2022-12-26 00:37:37 +0000

    settings2
    segments add
    1st section

--- myProject/src/app/tab4/tab4.page.ts
+++ myProject/src/app/tab4/tab4.page.ts
@@ -3,10 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
-  styleUrls: ['tab4.page.scss']
+  styleUrls: ['tab4.page.scss'],
 })
 export class Tab4Page {
-
+  darkModeIcon: string = 'moon';
+  selectedSegment: string = 'habAc';
   constructor() {}
+  segmentChanged(event: any) {
+    console.log(event.target.value);
+    this.selectedSegment = event.target.value;
+  }
+  toggleTheme(event: any) {
+    console.log(document.body.attributes);
+    if (event) {
+      if (document.body.attributes.length == 0) {
+        document.body.setAttribute('color-theme', 'dark');
+        this.darkModeIcon = this.darkModeIcon === 'moon' ? 'sunny' : 'moon';
+      } else {
+        document.body.removeAttribute('color-theme');
+        this.darkModeIcon = this.darkModeIcon === 'moon' ? 'sunny' : 'moon';
+      }
+    }
+  }
 
+  presentingElement = null;
+  ngOnInit() {
+    this.presentingElement = document.querySelector('.ion-page');
+  }
 }
